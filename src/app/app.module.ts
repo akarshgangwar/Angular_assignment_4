504c9be4b00319dfdcb6d846eaa5ab0d03128304
Akarsh Gangwar <akarsh.gangwar@stackpro.in>
2022-07-19 13:40:53 +0530

    initial setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameControlComponent } from './game-control/game-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameControlComponent
   ],
   imports: [
     BrowserModule
